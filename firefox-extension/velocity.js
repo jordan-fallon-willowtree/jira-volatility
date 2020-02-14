17b9083ea7b26bd7e2cc0dfa73a91b5805890096
Jordan Fallon <jordan.fallon@willowtreeapps.com>
2020-02-14 15:42:15 -0500

    Hit the Jira API with the power of cookies 🍪

--- firefox-extension/velocity.js
+++ firefox-extension/velocity.js
@@ -1,45 +1,5 @@
 console.log('Jira velocity loaded! 🐧')
 
-const velocityHelper = {
-    init(rows) {
-        this.issues = rows
-        return this
-    },
-
-    letsFuckingGo() {
-        const pointsByPlatform = this.issues
-            .reduce((result, next) => {
-                const platforms = next.querySelector('.ghx-plan-extra-fields .ghx-extra-field:last-child').innerText
-                const estimate = next.querySelector('.ghx-estimate .ghx-statistic-badge').innerText
-
-                if(estimate.trim().length) {
-                    if(platforms.includes("iOS") || platforms.includes("tvOS")) {
-                        result.iOS += Number(estimate)
-                    } else if(platforms.includes("Android") || platforms.includes("Amazon")) {
-                        result.android += Number(estimate)
-                    } else if(platforms.includes("Web")) {
-                        result.web += Number(estimate)
-                    }
-                }
-
-                return result
-            }, {android: 0, iOS: 0, web: 0})
-        
-        const badgeContainer = document.querySelector('.ghx-sprint-group .ghx-badge-group.ghx-right')
-        badgeContainer.prepend(
-            this.makeBadge(`android: ${pointsByPlatform.android}`),
-            this.makeBadge(`ios: ${pointsByPlatform.iOS}`),
-            this.makeBadge(`web: ${pointsByPlatform.web}`),
-        )
-    },
-
-    makeBadge(str) {
-        const badge = document.createElement('aui-badge')
-        badge.innerText = str
-        return badge
-    }
-}
-
 const pointField = 'customfield_10004'
 const sprintIds = [
     1405, // 28
@@ -73,32 +33,131 @@ function getSprints() {
         .then(res => res.json())
         .then(data => console.log(data.values.map(sprint => ({id: sprint.id, name: sprint.name}))))
 }
-// getSprints()
 
+const issues = []
 function breakdownSprint(sprintId) {
-    fetch(`${agileURL}/board/309/sprint/${sprintId}/issue`)
+    return fetch(`${agileURL}/board/309/sprint/${sprintId}/issue`)
         .then(res => res.json())
         .then(data => {
-            console.log(
-                data.issues
-                    .filter(issue => issue.fields.status.name == 'Done')
-                    .filter(issue => issue.fields.customfield_10004)
-                    .reduce((obj, issue) => {
-                        const points = issue.fields.customfield_10004
-                        if(isApple(issue)) {
-                            obj.apple += points
-                        } else if(isAndroid(issue)) {
-                            obj.android += points
-                        } else if(isWeb(issue)) {
-                            obj.web += points
-                        }
-                        return obj
-                    }, {android: 0, apple: 0, web: 0})
-            )
+            const validIssues = data.issues
+                .filter(issue => issue.fields.status.name == 'Done')
+                .filter(issue => issue.fields.customfield_10004)
+                .filter(issue => issue.fields.resolutiondate)
+
+            issues.push(...validIssues)
         })
 }
-sprintIds.forEach(breakdownSprint)
 
+function getPlatform(issue) {
+    if(isApple(issue)) { return 'iOS' }
+    else if(isAndroid(issue)) { return 'android' }
+    else if(isWeb(issue)) { return 'web' }
+    else { return 'qa' }
+}
+
+function figureOutVelocity() {
+    // getSprints()
+    const promises = []
+    sprintIds.forEach(sprintId => {
+        promises.push(breakdownSprint(sprintId))
+    })
+    Promise.all(promises).then(() => {
+        console.log(issues)
+        const dedupedIssues = [...new Set(issues
+            .map(issue => ({
+                    id: issue.id,
+                    points: issue.fields.customfield_10004, 
+                    done: new Date(issue.fields.resolutiondate),
+                    platform: getPlatform(issue)
+                }))
+            .sort((a, b) => a.done > b.done)
+        )]
+        // console.log(dedupedIssues)
+        // console.log(`total velocity: ${dedupedIssues.reduce((total, issue) => total + issue.points, 0 )}`)
+
+        const firstDate = dedupedIssues[0].done
+        const lastDate = dedupedIssues[dedupedIssues.length - 1].done
+        const millisecondsPerWeek = 1000 * 60 * 60 * 24 * 7
+        const numberOfIterations = Math.ceil((lastDate - firstDate) / millisecondsPerWeek)
+        console.log(`number of iterations: ${ numberOfIterations }`)
+
+        const iterations = []
+        for(i = 0; i < numberOfIterations; i++) {
+            iterations.push({ android: 0, iOS: 0, web: 0, qa: 0 })
+        }
+
+        const velocityByIteration = dedupedIssues
+            .reduce((result, issue) => {
+                const iterationIndex = ((issue.done - firstDate) / millisecondsPerWeek).toFixed()
+                result[iterationIndex][issue.platform] += issue.points
+                return result
+            },
+            iterations)
+        console.log(velocityByIteration)
+
+        console.log(`Android running velocity: ${velocityByIteration.reduce((total, iteration) => total + iteration.android, 0) / numberOfIterations}`)
+        console.log(`iOS running velocity: ${velocityByIteration.reduce((total, iteration) => total + iteration.iOS, 0) / numberOfIterations}`)
+        console.log(`Web running velocity: ${velocityByIteration.reduce((total, iteration) => total + iteration.web, 0) / numberOfIterations}`)
+        console.log(`QA running velocity: ${velocityByIteration.reduce((total, iteration) => total + iteration.qa, 0) / numberOfIterations}`)
+        // console.log(`total velocity again: ${velocityByIteration.reduce((total, iteration) => total + iteration.qa + iteration.android + iteration.iOS + iteration.web, 0)}`)
+
+        // console.log(dedupedIssues
+        //     .reduce((obj, issue) => {
+        //         if(isApple(issue)) {
+        //             obj.apple.push(issue)
+        //         } else if(isAndroid(issue)) {
+        //             obj.android.push(issue)
+        //         } else if(isWeb(issue)) {
+        //             obj.web.push(issue)
+        //         }
+        //         return obj
+        //     }, 
+        //     {android: [], apple: [], web: []})
+        // )
+    })
+}
+figureOutVelocity()
+
+
+// const velocityHelper = {
+//     init(rows) {
+//         this.issues = rows
+//         return this
+//     },
+
+//     letsFuckingGo() {
+//         const pointsByPlatform = this.issues
+//             .reduce((result, next) => {
+//                 const platforms = next.querySelector('.ghx-plan-extra-fields .ghx-extra-field:last-child').innerText
+//                 const estimate = next.querySelector('.ghx-estimate .ghx-statistic-badge').innerText
+
+//                 if(estimate.trim().length) {
+//                     if(platforms.includes("iOS") || platforms.includes("tvOS")) {
+//                         result.iOS += Number(estimate)
+//                     } else if(platforms.includes("Android") || platforms.includes("Amazon")) {
+//                         result.android += Number(estimate)
+//                     } else if(platforms.includes("Web")) {
+//                         result.web += Number(estimate)
+//                     }
+//                 }
+
+//                 return result
+//             }, {android: 0, iOS: 0, web: 0})
+        
+//         const badgeContainer = document.querySelector('.ghx-sprint-group .ghx-badge-group.ghx-right')
+//         badgeContainer.prepend(
+//             this.makeBadge(`android: ${pointsByPlatform.android}`),
+//             this.makeBadge(`ios: ${pointsByPlatform.iOS}`),
+//             this.makeBadge(`web: ${pointsByPlatform.web}`),
+//         )
+//     },
+
+//     makeBadge(str) {
+//         const badge = document.createElement('aui-badge')
+//         badge.innerText = str
+//         return badge
+//     }
+// }
 // function waitForJira() {
 //     let interval
 
