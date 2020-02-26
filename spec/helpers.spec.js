2c683d86034ee224cc3ac28ba48b8ea5343b0acd
Jordan Fallon <jordan.fallon@willowtreeapps.com>
2020-02-26 13:02:19 -0500

    Add test helpers

--- spec/helpers.spec.js
+++ spec/helpers.spec.js
@@ -69,9 +69,9 @@ describe('helpers', () => {
 
     describe('filterOutInvalidIssues', () => {
         it('filters out issues that are not Done', () => {
-            const inProgressIssue = { fields: { status: { name: 'In Progress' }, customfield_10004: 1, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
-            const doneIssue = { fields: { status: { name: 'Done' }, customfield_10004: 1, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
-            const inReviewIssue = { fields: { status: { name: 'In Review' }, customfield_10004: 1, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
+            const inProgressIssue = issueWithStatus('In Progress')
+            const doneIssue = issueWithStatus('Done')
+            const inReviewIssue = issueWithStatus('In Review')
 
             const validIssues = filterOutInvalidIssues([inProgressIssue, doneIssue, inReviewIssue])
             expect(validIssues.length).toBe(1)
@@ -79,24 +79,34 @@ describe('helpers', () => {
         })
 
         it('filters out issues that are not pointed', () => {
-            const pointedIssue = { fields: { status: { name: 'Done' }, customfield_10004: 3, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
-            const nullPointsIssue = { fields: { status: { name: 'Done' }, customfield_10004: null, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
-            const noPointsFieldIssue = { fields: { status: { name: 'Done' }, resolutiondate: '2019-01-01T14:25:20.000Z'  } }
-            const zeroPointsIssue = { fields: { status: { name: 'Done' }, customfield_10004: 0, resolutiondate: '2019-01-01T14:25:20.000Z' } }
+            const pointedIssue = issueWithPoints(3)
+            const nullPointsIssue = issueWithPoints(null)
+            const zeroPointsIssue = issueWithPoints(0)
 
-            const validIssues = filterOutInvalidIssues([pointedIssue, nullPointsIssue, noPointsFieldIssue, zeroPointsIssue])
+            const validIssues = filterOutInvalidIssues([pointedIssue, nullPointsIssue, zeroPointsIssue])
             expect(validIssues.length).toBe(1)
             expect(validIssues[0]).toBe(pointedIssue)
         })
 
         it('filters out issues that do not have a resolution date', () => {
-            const nullDateIssue = { fields: { status: { name: 'Done' }, customfield_10004: 3, resolutiondate: null } }
-            const noDateIssue = { fields: { status: { name: 'Done' }, customfield_10004: 3 } }
-            const validIssue = { fields: { status: { name: 'Done' }, customfield_10004: 3, resolutiondate: '2019-01-01T14:25:20.000Z' } }
+            const nullDateIssue = issueWithResolutionDate(null)
+            const validIssue = issueWithResolutionDate('2019-05-04T03:02:01.000Z')
 
-            const validIssues = filterOutInvalidIssues([nullDateIssue, noDateIssue, validIssue])
+            const validIssues = filterOutInvalidIssues([nullDateIssue, validIssue])
             expect(validIssues.length).toBe(1)
             expect(validIssues[0]).toBe(validIssue)
         })
     })
+
+    function issueWithStatus(status) {
+        return { fields: { status: { name: status }, customfield_10004: 1, resolutiondate: '2019-01-01T14:25:20.000Z' } }
+    }
+
+    function issueWithPoints(points) {
+        return { fields: { status: { name: 'Done' }, customfield_10004: points, resolutiondate: '2019-01-01T14:25:20.000Z' } }
+    }
+
+    function issueWithResolutionDate(date) {
+        return { fields: { status: { name: 'Done' }, customfield_10004: 1, resolutiondate: date } }
+    }
 })
