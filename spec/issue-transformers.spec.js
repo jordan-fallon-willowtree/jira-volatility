3065e3c75a95b2f5c461b65dee4192cc82a0f637
Jordan Fallon <jordan.fallon@willowtreeapps.com>
2020-02-28 13:37:17 -0500

    More transformer testing

--- spec/issue-transformers.spec.js
+++ spec/issue-transformers.spec.js
@@ -29,9 +29,33 @@ describe('Issue transformers', () => {
 
     describe('issueData', () => {
         it('returns the list of issues', () => {
-            const issues = [issueWithId(4), issueWithId(5)]
+            const issues = [{id: 5}, {id: 7}]
             const data = issueData(issues)
             expect(data.issues).toBe(issues)
+        })
+
+        it('returns the done date from the first issue, as they are sorted by date', () => {
+            const jan1 = new Date('2019-01-01T12:34:56.000Z')
+            const jan5 = new Date('2019-01-05T12:34:56.000Z')
+            const jan13 = new Date('2019-01-13T12:34:56.000Z')
+
+            const issues = [{done: jan1}, {done: jan5}, {done: jan13}]
+            const data = issueData(issues)
+
+            expect(data.firstDate).toBe(jan1)
+        })
+
+        it('returns the last date we care about, based on how many full iterations there are', () => {
+            const jan1 = new Date('2019-01-01T12:34:56.000Z')
+            const jan5 = new Date('2019-01-05T12:34:56.000Z')
+            const jan13 = new Date('2019-01-13T12:34:56.000Z')
+            const jan17 = new Date('2019-01-17T12:34:56.000Z')
+
+            const issues = [{done: jan1}, {done: jan5}, {done: jan13}, {done: jan17}]
+            const data = issueData(issues)
+
+            expect(data.numberOfFullIterations).toBe(2)
+            expect(data.lastDateWeCareAbout).toEqual(new Date('2019-01-15T12:34:56.000Z'))
         })
     })
 })
