1f3c872287d3c3ba587447e39a29ab8804fd7d32
Jordan Fallon <jordan.fallon@willowtreeapps.com>
2020-02-28 14:42:46 -0500

    Move functions around

--- firefox-extension/velocity.js
+++ firefox-extension/velocity.js
@@ -12,6 +12,25 @@ const sprintIds = [
     1424, // 32
 ]
 
+function figureOutVelocity() {
+    getValidSortedDedupedIssues(data => {
+        const initialTotalVelocity = totalPointsFromIssues(data.issues)
+        const iterations = buildIterations(data)
+        const decomposedTotalVelocity = totalPointsFromIterations(iterations)
+
+        if(decomposedTotalVelocity === initialTotalVelocity) {
+            console.log(`Android running velocity: ${platformVelocity(iterations, ANDROID)}`)
+            console.log(`Apple running velocity: ${platformVelocity(iterations, APPLE)}`)
+            console.log(`Web running velocity: ${platformVelocity(iterations, WEB)}`)
+            console.log(`TE running velocity: ${platformVelocity(iterations, TE)}`)
+
+            addToDOM(iterations)
+        } else {
+            console.log(`Something went wrong! Our initial total velocity of ${initialTotalVelocity} wasn't reflected in the final calculation. Instead, we got ${decomposedTotalVelocity}`)
+        }
+    })
+}
+
 function getValidSortedDedupedIssues(callback) {
     getIssuesForSprints(issues => {
         const mappedSortedFilteredIssues = filterOutInvalidIssues(issues)
@@ -34,54 +53,37 @@ function getIssuesForSprints(callback) {
         })
 }
 
-function addToDOM(velocityByIteration) {
+function buildIterations(data) {
+    const iterations = []
+    for(i = 0; i < data.numberOfFullIterations; i++) {
+        iterations.push({ [ANDROID]: 0, [APPLE]: 0, [WEB]: 0, [TE]: 0, issueIds: [] })
+    }
+
+    return data.issues
+        .reduce((result, issue) => {
+            const iterationIndex = Math.floor((issue.done - data.firstDate) / MILLISECONDS_PER_WEEK)
+            issue.platforms.forEach(platform => result[iterationIndex][platform] += issue.points)
+            result[iterationIndex].issueIds.push(issue.id)
+            return result
+        },
+        iterations)
+}
+
+function addToDOM(iterations) {
     const newDiv = document.createElement('div')
     newDiv.id = 'velocity-helper'
     newDiv.innerHTML = `
         <div style="font-size: 18px; font-weight: bold;">
             Velocity <i>(avg points / week for the last 4 sprints)</i>:
-            <span style="color: #A4C639;">Android - ${platformVelocity(velocityByIteration, ANDROID)}</span> |
-            <span style="color: #7D7D7D;">Apple - ${platformVelocity(velocityByIteration, APPLE)}</span> |
-            <span style="color: #007ACC;">Web - ${platformVelocity(velocityByIteration, WEB)}</span> |
-            <span style="color: #009800;">TE - ${platformVelocity(velocityByIteration, TE)}</span>
+            <span style="color: #A4C639;">Android - ${platformVelocity(iterations, ANDROID)}</span> |
+            <span style="color: #7D7D7D;">Apple - ${platformVelocity(iterations, APPLE)}</span> |
+            <span style="color: #007ACC;">Web - ${platformVelocity(iterations, WEB)}</span> |
+            <span style="color: #009800;">TE - ${platformVelocity(iterations, TE)}</span>
         </div>
     `
 
     const pageContainer = document.getElementById('gh')
     pageContainer.prepend(newDiv)
-}
-
-function figureOutVelocity() {
-    getValidSortedDedupedIssues(data => {
-        const initialTotalVelocity = totalPointsFromIssues(data.issues)
-
-        const iterations = []
-        for(i = 0; i < data.numberOfFullIterations; i++) {
-            iterations.push({ [ANDROID]: 0, [APPLE]: 0, [WEB]: 0, [TE]: 0, issueIds: [] })
-        }
-
-        const velocityByIteration = data.issues
-            .reduce((result, issue) => {
-                const iterationIndex = Math.floor((issue.done - data.firstDate) / MILLISECONDS_PER_WEEK)
-                issue.platforms.forEach(platform => result[iterationIndex][platform] += issue.points)
-                result[iterationIndex].issueIds.push(issue.id)
-                return result
-            },
-            iterations)
-        // console.log(velocityByIteration)
-
-        const decomposedTotalVelocity = totalPointsFromIterations(velocityByIteration)
-        if(decomposedTotalVelocity === initialTotalVelocity) {
-            console.log(`Android running velocity: ${platformVelocity(velocityByIteration, ANDROID)}`)
-            console.log(`Apple running velocity: ${platformVelocity(velocityByIteration, APPLE)}`)
-            console.log(`Web running velocity: ${platformVelocity(velocityByIteration, WEB)}`)
-            console.log(`TE running velocity: ${platformVelocity(velocityByIteration, TE)}`)
-
-            addToDOM(velocityByIteration)
-        } else {
-            console.log(`Something went wrong! Our initial total velocity of ${initialTotalVelocity} wasn't reflected in the final calculation. Instead, we got ${decomposedTotalVelocity}`)
-        }
-    })
 }
 
 module.exports = { figureOutVelocity }
