212147af02f4a9f5029cee7ed3812ae2258cad59
Jordan Fallon <jordan.fallon@willowtreeapps.com>
2020-03-02 16:05:13 -0500

    Add sprint 33, remove 29

--- firefox-extension/velocity.js
+++ firefox-extension/velocity.js
@@ -7,10 +7,11 @@ const { AgileCalculator } = require('./agile-calculator.js')
 
 const sprintIds = [
     // 1405, // 28
-    1406, // 29
+    // 1406, // 29
     1414, // 30
     1422, // 31
     1424, // 32
+    1436, // 33
 ]
 
 function figureOutVelocity() {
